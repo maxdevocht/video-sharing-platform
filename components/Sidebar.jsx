3afde08ce52d1085697fa8534dfcd8fad020d660
Max de Vocht <maxdevocht@hotmail.com>
2024-04-05 13:36:29 +0200

    Added Sidebar.jsx

---
+++ components/Sidebar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+const sidebarItems = [
+    { icon: 'ri-home-4-line', label: 'Dashboard' },
+    { icon: 'ri-user-6-line', label: 'My Channel' },
+    { icon: 'ri-history-line', label: 'History' },
+    { icon: 'ri-movie-2-line', label: 'Popular' },
+    { icon: 'ri-film-line', label: 'Playlist' },
+    { icon: 'ri-chat-1-line', label: 'Messages' },
+    { icon: 'ri-thumb-up-line', label: 'Liked Videos' },
+    { icon: 'ri-settings-3-line', label: 'Settings' },
+];
+
+export default function Sidebar() {
+    const [activeItem, setActiveItem] = useState(0); // Initial active item is the first one (Dashboard)
+
+    const handleItemClick = (index) => {
+        setActiveItem(index);
+    };
+
+    return (
+        <aside className="left-section">
+            <div className="logo">
+                <img src="" alt="" />
+            </div>
+
+            <div className="sidebar">
+                {sidebarItems.map((sideItem, index) => (
+                <div
+                    key={index}
+                    className={`sidebar-item ${activeItem === index ? 'active' : ''}`}
+                    onClick={() => handleItemClick(index)}
+                >
+                    <i className={sideItem.icon}></i>
+                    <h3>{sideItem.label}</h3>
+                </div>
+                ))}
+            </div>
+
+            <div className="sign-out">
+                <i className="ri-logout-box-r-line"></i>
+                <h3>Sign Out</h3>
+            </div>
+        </aside>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import Sidebar from '../components/Sidebar'
 
 export default function App() {
   return (
     <>
+      <Sidebar />
     </>
   )
 }
